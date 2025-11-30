a053a77038146bc93ee8507d9cfdfa5447b2e27c
adarpal07 <adarpal.work.07@gmail.com>
2025-11-30 12:06:26 +0600

    changed launcher

--- src/main/java/mid/gulshan_club/Launcher.java
+++ src/main/java/mid/gulshan_club/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.out.println('H');
         Application.launch(HelloApplication.class, args);
     }
 }
