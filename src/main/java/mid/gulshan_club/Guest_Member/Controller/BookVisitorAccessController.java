b554708081890e2ba1c201812e58d22f6d93c0b1
SOHAN470 <sohanmahmud147@gmail.com>
2025-12-04 13:03:07 -0800

    Modified: - BookVisitorAccessController.java - SupplierDashboardController.java - BookVisitorAccess-view.fxml - supplierDashboard-View.fxml
    Updated controller logic and synchronized UI layout with new functionality.

--- src/main/java/mid/gulshan_club/Guest_Member/Controller/BookVisitorAccessController.java
+++ src/main/java/mid/gulshan_club/Guest_Member/Controller/BookVisitorAccessController.java
@@ -1,4 +1,25 @@
 package mid.gulshan_club.Guest_Member.Controller;
 
+import javafx.scene.control.DatePicker;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class BookVisitorAccessController {
+    @javafx.fxml.FXML
+    private TextField nidNumberTextfield;
+    @javafx.fxml.FXML
+    private TextField contactNumberTextfield;
+    @javafx.fxml.FXML
+    private TextArea purposeOfVisitTextArea;
+    @javafx.fxml.FXML
+    private TextField emailTextfield;
+    @javafx.fxml.FXML
+    private DatePicker visitDatePicker;
+    @javafx.fxml.FXML
+    private TextField nameTextfield;
+
+    @javafx.fxml.FXML
+    public void submitSaveButton(ActionEvent actionEvent) {
+    }
 }
