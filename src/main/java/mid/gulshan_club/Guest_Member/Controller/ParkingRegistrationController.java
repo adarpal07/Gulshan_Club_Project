e588aad21f5326f59a80de97bbe12a4e4f1a5381
SOHAN470 <sohanmahmud147@gmail.com>
2025-12-05 06:41:29 -0800

    Updated Guest Member modules: Parking Registration, Private Event Request, Special Food Request, and related FXML views.

--- src/main/java/mid/gulshan_club/Guest_Member/Controller/ParkingRegistrationController.java
+++ src/main/java/mid/gulshan_club/Guest_Member/Controller/ParkingRegistrationController.java
@@ -1,7 +1,36 @@
 package mid.gulshan_club.Guest_Member.Controller;
 
+import javafx.scene.control.ComboBox;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class ParkingRegistrationController
 {
     @javafx.fxml.FXML
+    private TextField modelTextfield;
+    @javafx.fxml.FXML
+    private TextField memberIdTextfield;
+    @javafx.fxml.FXML
+    private ComboBox parkingDurationComboBox;
+    @javafx.fxml.FXML
+    private Button backButton;
+    @javafx.fxml.FXML
+    private TextField licensePlateNumberTextfield;
+    @javafx.fxml.FXML
+    private TextField brandTextfield;
+    @javafx.fxml.FXML
+    private TextField nameTextfield;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void registarButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
