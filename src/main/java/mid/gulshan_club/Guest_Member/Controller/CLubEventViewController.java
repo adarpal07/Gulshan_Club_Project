c68520b04002286dc5bf66220b878383055daded
SOHAN470 <sohanmahmud147@gmail.com>
2025-12-05 04:05:58 -0800

    Guest Member Module Update: - Updated BookVisitorAccessController - Updated ClubEventViewController - Updated FacilityBookingController - Updated related FXML views (BookVisitorAccess, ClubEvents, FacilityBooking)

--- src/main/java/mid/gulshan_club/Guest_Member/Controller/CLubEventViewController.java
+++ src/main/java/mid/gulshan_club/Guest_Member/Controller/CLubEventViewController.java
@@ -1,7 +1,47 @@
 package mid.gulshan_club.Guest_Member.Controller;
 
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+
+import javax.swing.table.TableColumn;
+import javax.swing.text.TableView;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class CLubEventViewController
 {
     @javafx.fxml.FXML
+    private TableColumn timeColumn;
+    @javafx.fxml.FXML
+    private TextField eventNameTextfield;
+    @javafx.fxml.FXML
+    private TableColumn locationColumn;
+    @javafx.fxml.FXML
+    private TableView ClubEventTableview;
+    @javafx.fxml.FXML
+    private TableColumn evetNameColumn;
+    @javafx.fxml.FXML
+    private TextField timeTextField;
+    @javafx.fxml.FXML
+    private TableColumn dateColumn;
+    @javafx.fxml.FXML
+    private ComboBox locationCombobox;
+    @javafx.fxml.FXML
+    private DatePicker dateDatepicker;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void refreshButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void requestToAttendButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backToDashboardbutton(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/mid/gulshan_club/Guest_Member/Controller/FacilityBookingController.java
+++ src/main/java/mid/gulshan_club/Guest_Member/Controller/FacilityBookingController.java
@@ -1,7 +1,48 @@
 package mid.gulshan_club.Guest_Member.Controller;
 
+import javafx.scene.control.ComboBox;
+
+import javax.swing.table.TableColumn;
+import javax.swing.text.TableView;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class FacilityBookingController
 {
     @javafx.fxml.FXML
+    private TableColumn timeslotColumn;
+    @javafx.fxml.FXML
+    private TableColumn availableSeatColumn;
+    @javafx.fxml.FXML
+    private TableColumn facilityNameColumn;
+    @javafx.fxml.FXML
+    private Button backButton;
+    @javafx.fxml.FXML
+    private TextField seatCountTextField;
+    @javafx.fxml.FXML
+    private TextField timeSlotTextField;
+    @javafx.fxml.FXML
+    private TextField facilityNameTextField;
+    @javafx.fxml.FXML
+    private TableView clFacilityBookingTableView;
+    @javafx.fxml.FXML
+    private TableColumn avalibilityColumn;
+    @javafx.fxml.FXML
+    private ComboBox avalibilityComboBox;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void refreshButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void bookButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
