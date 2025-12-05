2c049624b97fe2998fbb9de72f91edee29e47cc0
AdityaKar23 <adityakar141@gmail.com>
2025-12-05 18:14:57 +0600

    Worked for Accountant i

--- src/main/java/mid/gulshan_club/Accountant/Controller/RecordMembershipFeeController.java
+++ src/main/java/mid/gulshan_club/Accountant/Controller/RecordMembershipFeeController.java
@@ -9,9 +9,13 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import mid.gulshan_club.Accountant.Model.RecordMembershipFee;
 import mid.gulshan_club.HelloApplication;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class RecordMembershipFeeController
 {
@@ -36,7 +40,62 @@ public class RecordMembershipFeeController
 
     @javafx.fxml.FXML
     public void recordPaymentButton(ActionEvent actionEvent) {
+
+        if (nameTextfield.getText().isBlank()) {
+            errorAlert("Member name cannot be blank!");
+            return;
+        }
+        if (idTextfield.getText().isBlank()) {
+            errorAlert("Member ID cannot be blank!");
+            return;
+        }
+        if (amountTextfield.getText().isBlank()) {
+            errorAlert("Amount cannot be blank!");
+            return;
+        }
+        if (statusComboBox.getValue() == null) {
+            errorAlert("Please select a status!");
+            return;
+        }
+        if (monthComboBox.getValue() == null) {
+            errorAlert("Please select a month!");
+            return;
+        }
+        if (recordDatePicker.getValue() == null) {
+            errorAlert("Please select a record date!");
+            return;
+        }
+        RecordMembershipFee rmf = new RecordMembershipFee(
+                nameTextfield.getText(),
+                statusComboBox.getValue(),
+                monthComboBox.getValue(),
+                Integer.parseInt(idTextfield.getText()),
+                Integer.parseInt(amountTextfield.getText()),
+                recordDatePicker.getValue()
+        );
+        try {
+
+            File file = new File("RecordMembershipFee.bin");
+            FileOutputStream fos;
+            ObjectOutputStream oos;
+
+            if (file.exists()) {
+                fos = new FileOutputStream(file, true);
+                oos = new AppendableObjectOutputStream(fos);
+            } else {
+                fos = new FileOutputStream(file);
+                oos = new ObjectOutputStream(fos);
+            }
+            oos.writeObject(rmf);
+            oos.close();
+            informationAlert("Membership fee record saved successfully!");
+
+        } catch (Exception e) {
+            errorAlert("Error saving data to file!");
+        }
     }
+
+
 
     @javafx.fxml.FXML
     public void backButton(ActionEvent actionEvent) throws IOException {
--- src/main/java/mid/gulshan_club/Accountant/Model/GeneratePaymentRecords.java
+++ src/main/java/mid/gulshan_club/Accountant/Model/GeneratePaymentRecords.java
@@ -2,25 +2,10 @@ package mid.gulshan_club.Accountant.Model;
 
 import mid.gulshan_club.Employee;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class GeneratePaymentRecords extends Employee {
-    public GeneratePaymentRecords(long userId) {
-        super(userId);
-    }
+public class GeneratePaymentRecords implements Serializable {
 
-    @Override
-    public String toString() {
-        return "GeneratePaymentRecords{" +
-                "joiningDate=" + joiningDate +
-                ", salary=" + salary +
-                ", duration=" + duration +
-                ", department='" + department + '\'' +
-                ", userName='" + userName + '\'' +
-                ", phoneNo=" + phoneNo +
-                ", userId=" + userId +
-                ", gender='" + gender + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+
 }
