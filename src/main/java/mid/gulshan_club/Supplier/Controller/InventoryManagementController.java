1b0578211f833f065f68054af224aba7be7432dc
SOHAN470 <sohanmahmud147@gmail.com>
2025-12-05 10:56:55 -0800

    Update Supplier and Guest Member modules
    - Updated controllers: PolicyDocumentViewController, InventoryManagementController,
      InvoiceManagementController, SupplierProfileViewController, TrainingSuppliesViewController
    - Updated FXML views for Guest Member policy documents and all Supplier modules:
      InventoryManagement, InvoiceManagement, SupplierProfile, TrainingSupplies

--- src/main/java/mid/gulshan_club/Supplier/Controller/InventoryManagementController.java
+++ src/main/java/mid/gulshan_club/Supplier/Controller/InventoryManagementController.java
@@ -1,7 +1,49 @@
 package mid.gulshan_club.Supplier.Controller;
 
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+
+import javax.swing.table.TableColumn;
+import javax.swing.text.TableView;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class InventoryManagementController
 {
     @javafx.fxml.FXML
+    private TableColumn itemNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn unitColumn;
+    @javafx.fxml.FXML
+    private Button submitButton;
+    @javafx.fxml.FXML
+    private TableColumn catagoryColumn;
+    @javafx.fxml.FXML
+    private TableColumn expDateColumn;
+    @javafx.fxml.FXML
+    private Button backButton;
+    @javafx.fxml.FXML
+    private TextField unitTextfield;
+    @javafx.fxml.FXML
+    private ComboBox catagoryComboBox;
+    @javafx.fxml.FXML
+    private ComboBox expDateCombobox;
+    @javafx.fxml.FXML
+    private TableView inventoryTableview;
+    @javafx.fxml.FXML
+    private TextField itemNameTextfield;
+    @javafx.fxml.FXML
+    private DatePicker dateDatepicker;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void submitButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/mid/gulshan_club/Supplier/Controller/TrainingSuppliesViewController.java
+++ src/main/java/mid/gulshan_club/Supplier/Controller/TrainingSuppliesViewController.java
@@ -1,7 +1,39 @@
 package mid.gulshan_club.Supplier.Controller;
 
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class TrainingSuppliesViewController
 {
     @javafx.fxml.FXML
+    private DatePicker proposedeliveryDatetoChefDatepicker;
+    @javafx.fxml.FXML
+    private TextField quantityTextfield;
+    @javafx.fxml.FXML
+    private ComboBox trainingDateComboBox;
+    @javafx.fxml.FXML
+    private Button backButton;
+    @javafx.fxml.FXML
+    private TextField poNoTextfield;
+    @javafx.fxml.FXML
+    private TextField itemTextField;
+    @javafx.fxml.FXML
+    private TextField kitchenLocatTextfield;
+    @javafx.fxml.FXML
+    private Button confirmButton;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmButton(ActionEvent actionEvent) {
+    }
+}
